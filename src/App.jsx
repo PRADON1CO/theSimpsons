2e6274d6ea1e6da2e21cc22328d3655feb096af9
Prado Nicolas <nicolas88prado@gmail.com>
2024-04-24 21:03:36 -0300

    mostramos la respuesta de la api en componente Frase

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
           src="https://media-protected.taiga.io/attachments/a/a/b/4/1e59bddbfc9b0d9e56fadf4f4230a3c93b20423105e3afc205e3b0a1e4b8/logosimpson.png?token=ZilhpQ%3AJw7W1pZNBDmmVkGlVuaIMuSCkcaoEeST5whd1Wlmn6y2VF2Uzzevbm0_x4a4ftyTotwbEJpzJbcVVJeNm64MGw#_taiga-refresh=userstory:2494482"
           alt=""
         />
-        <Frase></Frase>
+        <Frase frasePersonaje={FrasePersonaje}></Frase>
         <Button className="m-2 btn-dark">Obtener Frase</Button>
       </Container>
     </>
